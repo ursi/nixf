c0ba938d4cff661a54622e9e7306d5068dc71140
Mason Mackaman <masondeanm@aol.com>
2021-04-18 23:37:42 -0400

    Add version

--- default.nix
+++ default.nix
@@ -1,4 +1,20 @@
 { pkgs ? import <nixpkgs> {} }:
-  pkgs.writeShellScriptBin "nixf"
-    ''${pkgs.nixUnstable}/bin/nix --experimental-features "nix-command flakes" $@''
+  let
+    inherit (pkgs) nixUnstable;
+
+    nixf =
+      pkgs.writeShellScript "nixf"
+        ''${nixUnstable}/bin/nix --experimental-features "nix-command flakes" $@'';
+  in
+  pkgs.stdenv.mkDerivation
+    { pname = "nixf";
+      inherit (nixUnstable) version;
+      phases = [ "installPhase" ];
+
+      installPhase =
+        ''
+        mkdir -p $out/bin
+        ln -s ${nixf} $out/bin/nixf
+        '';
+    }
 
