4601814ccbd4587ac19f8efeb75154c797175dc6
Mason Mackaman <masondeanm@aol.com>
2021-04-19 14:03:37 -0400

    Add simple bash completion

--- default.nix
+++ default.nix
@@ -5,6 +5,39 @@
     nixf =
       pkgs.writeShellScript "nixf"
         ''${nixUnstable}/bin/nix --experimental-features "nix-command flakes" $@'';
+
+    completions =
+      builtins.toString
+        [ "build"
+          "develop"
+          "flake"
+          "help"
+          "profile"
+          "repl"
+          "run"
+          "search"
+          "shell"
+          "bundle"
+          "copy"
+          "edit"
+          "eval"
+          "log"
+          "path-info"
+          "registry"
+          "why-depends"
+          "daemon"
+          "describe-stores"
+          "hash"
+          "key"
+          "nar"
+          "print-dev-env"
+          "realisation"
+          "show-config"
+          "show-derivation"
+          "store"
+          "doctor"
+          "upgrade-nix"
+        ];
   in
   pkgs.stdenv.mkDerivation
     { pname = "nixf";
@@ -14,7 +47,11 @@
       installPhase =
         ''
         mkdir -p $out/bin
-        ln -s ${nixf} $out/bin/nixf
+        cd $out
+        ln -s ${nixf} bin/nixf
+        mkdir -p share/bash-completion/completions
+        cd $_
+        echo 'complete -W "${completions}" nixf' > nixf
         '';
     }
 
